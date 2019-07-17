3033225438921aadacecba2e69f389a353776206
jamie <jamie.parkinson@techwillsaveus.com>
2019-07-17 23:50:25 +0100

    Remove remains of boilerplate

--- examples/simple/main/main.cpp
+++ examples/simple/main/main.cpp
@@ -1,17 +1,6 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <esp_log.h>
-#include <stdio.h>
-#include "esp_spi_flash.h"
-#include "esp_system.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 #include "Max31865.h"
 
